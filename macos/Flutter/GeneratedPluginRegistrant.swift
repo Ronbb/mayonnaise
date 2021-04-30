78132df1b9c5eff7c99342c10f2e986c47599052
ronbb <ronbiaobiao@gmail.com>
2021-04-30 17:34:17 +0800

    🛫 generate platform codes

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import entry
+import path_provider_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  EntryPlugin.register(with: registry.registrar(forPlugin: "EntryPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
